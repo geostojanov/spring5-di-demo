6fb1dfdd85236e7adbd4dfcd825d4b3de4d72996
geostojanov <gestojanov@gmail.com>
2019-09-16 16:01:49 +0200

    added external properties

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,9 +1,7 @@
 package guru.springframework;
 
-import guru.springframework.controllers.CIController;
 import guru.springframework.controllers.MyController;
-import guru.springframework.controllers.PIController;
-import guru.springframework.controllers.SIController;
+import guru.springframework.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,9 +13,10 @@ public class DiDemoApplication {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 		MyController controller = (MyController) ctx.getBean("myController");
 
-		System.out.println(controller.hello());
-		System.out.println(ctx.getBean(PIController.class).sayHello());
-		System.out.println(ctx.getBean(SIController.class).sayHello());
-		System.out.println(ctx.getBean(CIController.class).sayHello());
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+		System.out.println(fakeDataSource.getUser());
+		System.out.println(fakeDataSource.getPassword());
+		System.out.println(fakeDataSource.getUrl());
 	}
 }
