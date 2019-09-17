314b2db412308f0c65fe01b7d0eeef19f2f55140
geostojanov <gestojanov@gmail.com>
2019-09-17 12:38:35 +0200

    added multiple property files

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,17 +1,22 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
  * Created by jt on 6/7/17.
  */
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources(
+        {@PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")})
 public class PropertyConfig {
 
     @Value("${guru.username}")
@@ -22,6 +27,24 @@ public class PropertyConfig {
 
     @Value("${guru.dburl}")
     String url;
+
+    @Value("${guru.jms.username}")
+    String jmsUsername;
+
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+
+    @Value("${guru.jms.url}")
+    String jsmUrl;
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUsername(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jsmUrl);
+        return fakeJmsBroker;
+    }
 
     @Bean
     public FakeDataSource fakeDataSource(){
